a1953b9ca82a7250e9b601d5c735f93adf58f161
sukipannu <sukipannu@ymail.com>
2022-11-18 22:48:52 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const app =  express();
+const express = require('express'); 
+const PORT = process.env.PORT || 3001;
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+//retrieve css and js files
+
+//JSON data
+
+//PORT 
+
+module.exports = app;
