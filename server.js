ec09b9cfdcb197621117476ecb702d492281cb8b
sukipannu <sukipannu@ymail.com>
2022-11-20 23:43:07 -0500

    get started button works

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
 //retrieve css and js files
+app.use(express.static("public"));
 
 //JSON data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+//app.use(express.static('public'));
 
 //PORT 
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+});
 
 module.exports = app;
